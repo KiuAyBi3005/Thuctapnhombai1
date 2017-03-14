f0fce0594adea1fe11c28a723cfb7d0edd14cb72
hdhuy179 <vn01639519315@gmail.com>
2017-03-14 09:33:08 +0700

    Update TimKiem

--- 1.1 QuanLyNhanSu/MainTimKiem.cs
+++ 1.1 QuanLyNhanSu/MainTimKiem.cs
@@ -12,7 +12,56 @@ namespace _1._1_QuanLyNhanSu
     {
         public void action_TimKiem(frmMain frm)
         {
-            
+            string sl = "";
+            try
+            {
+                SqlConnection con = new SqlConnection(globalParemeter.connectionString);
+                con.Open();
+                string gt = "";
+                if (frm.cbGioiTinh.Checked == true)
+                {
+                    if (frm.rbtnNam.Checked) gt = "Nam";
+                    else if (frm.rbtnNu.Checked) gt = "Nu";
+                    sl = sl + " and GioiTinh = '" + gt + "'";
+                }
+                if (frm.cbCongViec.Checked == true)
+                {
+                    sl = sl + " and cv.MaCV=" + frm.cbbCongViec.SelectedValue.ToString();
+                }
+                if (frm.cbPhongBan.Checked == true)
+                {
+                    sl = sl + " and pb.MaPB=" + frm.cbbPhongBan.SelectedValue.ToString();
+                }
+                if (frm.cbTrinhDo.Checked == true)
+                {
+                    sl = sl + " and td.MaTD=" + frm.cbbTrinhDo.SelectedValue.ToString();
+                }
+                if (frm.txtMaNV.Text != "")
+                {
+                    sl = sl + " and nv.MaNV = " + frm.txtMaNV.Text;
+                }
+                if (frm.txtTenNV.Text != "")
+                {
+                    sl = sl + " and TenNV = '" + frm.txtTenNV.Text + "'";
+                }
+                if (frm.txtQueQuan.Text != "")
+                {
+                    sl = sl + " and QueQuan = '" + frm.txtQueQuan.Text + "'";
+                }
+                if (frm.txtBaoHiemXH.Text != "")
+                {
+                    sl = sl + " and BaoHiemXh = '" + frm.txtBaoHiemXH.Text + "'";
+                }
+                if (frm.txtSDT.Text != "")
+                {
+                    sl = sl + " and SoDt = '" + frm.txtSDT.Text + "'";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi chưa Thêm được");
+            }
+            frm.getData(sl);
         }
 
     }
